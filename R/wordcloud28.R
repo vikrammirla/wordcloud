731497fc6a052a258de3902e4c0a5d1a7eab794c
Vikram Mirla <vikrammirla@gmail.com>
2016-01-19 08:33:02 -0400

    added wordcloud after testing

--- R/wordcloud28.R
+++ R/wordcloud28.R
@@ -9,10 +9,130 @@
 #' @export
 wordcloud28 <- function() {
 
-	library(Quandl)
-	Quandl.api_key("pk6yzCsxF1k-ygB29nfS")
-	data<-Quandl(c("NS1/CSCO_MM"), trim_start="2014-11-13")
-	plot(data[,4])
+
+	library(tm)
+	cname <- file.path(".", "texts")
+	
+
+
+	dir(cname)
+	docs <- Corpus(DirSource(cname))  
+	inspect(docs)
+	docs$content
+
+	docs <- tm_map(docs, removePunctuation)   
+
+	for(j in seq(docs))   
+	{   
+	  docs[[j]] <- gsub("/", " ", docs[[j]])   
+	  docs[[j]] <- gsub("@", " ", docs[[j]])   
+	  docs[[j]] <- gsub("\\|", " ", docs[[j]])
+	  docs[[j]] <- gsub("\\|", " ", docs[[j]])
+	  docs[[j]] <- gsub("\\t", " ", docs[[j]])
+	  docs[[j]] <- gsub("\\]", " ", docs[[j]])
+	  docs[[j]] <- gsub("\\[", " ", docs[[j]])
+	} 
+
+	docs <- tm_map(docs, removeNumbers) 
+	docs <- tm_map(docs, tolower)   
+	docs <- tm_map(docs, removeWords, stopwords("english"))   
+	docs <- tm_map(docs, removeWords, c("department", "email"))
+
+
+	#Removing particular words:
+	docs <- tm_map(docs, removeWords, c("initiation", "email","june","bfams")) 
+	inspect(docs)
+	#Combining words that should stay together
+
+
+	for (j in seq(docs))
+	{
+	  docs[[j]] <- gsub("qualitative research", "QDA", docs[[j]])
+	  docs[[j]] <- gsub("goldman sachs global investment research", "GSGIR", docs[[j]])
+	  docs[[j]] <- gsub("qualitative analysis", "QDA", docs[[j]])
+	  docs[[j]] <- gsub("research methods", "research_methods", docs[[j]])
+	}
+
+
+	#Removing common word endings. (e.g., “ing”, “es”, “s”)
+	library(SnowballC)  
+	#docs <- tm_map(docs, stemDocument)   
+
+	#Stripping unnecesary whitespace from your documents
+	docs <- tm_map(docs, stripWhitespace)   
+
+
+
+	#This tells R to treat your preprocessed documents as text documents.
+	docs <- tm_map(docs, PlainTextDocument) 
+
+
+
+	#create a document term matrix.
+	dtm <- DocumentTermMatrix(docs)   
+	inspect(dtm)
+	dim(dtm)
+	inspect(dtm[1, 1:20])
+
+
+
+
+	#transpose of this matrix
+	tdm <- TermDocumentMatrix(docs)  
+
+
+	#Organize terms by their frequency:
+
+	colSums(as.matrix(as.matrix(dtm)))
+	freq <- colSums(as.matrix(dtm))   
+	length(freq)  
+	ord <- order(freq)
+	m <- as.matrix(dtm)   
+	dim(m) 
+	write.csv(m, file="dtm.csv") 
+
+
+
+	#  Start by removing sparse terms:
+	dtms <- removeSparseTerms(dtm, 0.3)
+	inspect(dtms)
+
+
+	#Word Frequency
+	freq[tail(ord)]
+	head(table(freq), 20) 
+	tail(table(freq), 20)
+
+	freq <- colSums(as.matrix(dtms))   
+	freq   
+	freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)   
+	head(freq, 14) 
+
+
+	findFreqTerms(dtm, lowfreq=20) 
+
+	wf <- data.frame(word=names(freq), freq=freq)   
+	head(wf)
+
+	library(ggplot2)   
+	#p <- ggplot(subset(wf, freq>15), aes(word, freq))    
+	#p <- p + geom_bar(stat="identity")   
+	#p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))   
+	#p   
+
+
+
+
+
+
+	#Relationships Between Terms
+	findAssocs(dtm, c("revenue" , "revenue"), corlimit=0.08)
+
+
+	library(wordcloud)   
+	wordcloud(names(freq), freq, min.freq=10)  
+
+
     invisible();
 }
 
